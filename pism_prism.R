aca52bfe219d0853ed2be9babc762e8b9feda3cf
Matheus Valentim <75315649+mavalentim@users.noreply.github.com>
2021-06-28 18:03:41 -0300

    Começando a analisar essa historia de PISM e efeitos

---
+++ pism_prism.R
@@ -0,0 +1,10 @@
+#descobrindo notas do PISM
+
+library(pdftools)
+library(tidyverse)
+install.packages('tesseract')
+
+('https://www2.ufjf.br/copese/wp-content/uploads/sites/42/2018/04/PISM-2019-ponto-de-corte-PISM-3.pdf')
+
+links: 
+  https://www2.ufjf.br/noticias/2020/10/30/confira-pontos-de-corte-das-edicoes-passadas-do-pism/
